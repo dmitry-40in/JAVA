2d34124fe1d0b48df8df53978029858ec5fc4153
Dmitry <dmitry_40in@mail.ru>
2023-02-14 13:51:59 +0300

    sem1

---
+++ program.java
@@ -0,0 +1,15 @@
+public class program { // это классический комметарий
+    public static void main(String[] args) {
+        char ch = '+';
+        System.out.println(ch);
+    }
+
+}
+
+/*
+это 
+многострочный
+комментарий
+*/
+
+
