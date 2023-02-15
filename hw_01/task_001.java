9de54c0e900357dd4333ded829b9a2193098ff09
Dmitry <dmitry_40in@mail.ru>
2023-02-15 13:15:27 +0300

    method 'calculationOfTNumber' added

--- hw_01/task_001.java
+++ hw_01/task_001.java
@@ -5,16 +5,23 @@ import java.util.Scanner;
 public class task_001 {
     public static void main(String[] args) {
         // int n = inputNumber();
-        // System.out.print(n);
-       
-    }    
+        // System.out.println(n);
+        // int t = calculationOfTNumber(n);
+        // System.out.println(t);
 
-    static int inputNumber () {
+    }
+
+    static int inputNumber() {
         Scanner iScanner = new Scanner(System.in);
         System.out.println("Для расчета n-ого треугольного числа необходимо задать n.");
         System.out.printf("n = ");
         int number = iScanner.nextInt();
-        iScanner.close(); 
+        iScanner.close();
         return number;
+    }
+
+    static int calculationOfTNumber(int n) {
+        int tNumber = (n * (n + 1)) / 2;
+        return tNumber;
     }
 }
