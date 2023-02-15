a3477992aaabdc4592d311de3af705b3a5aa4156
Dmitry <dmitry_40in@mail.ru>
2023-02-15 13:08:05 +0300

    method 'inputNumber' added

--- hw_01/task_001.java
+++ hw_01/task_001.java
@@ -1,8 +1,20 @@
 // Написать программу вычисления n-ого треугольного числа.
 
+import java.util.Scanner;
+
 public class task_001 {
     public static void main(String[] args) {
-
-        
+        // int n = inputNumber();
+        // System.out.print(n);
+       
     }    
+
+    static int inputNumber () {
+        Scanner iScanner = new Scanner(System.in);
+        System.out.println("Для расчета n-ого треугольного числа необходимо задать n.");
+        System.out.printf("n = ");
+        int number = iScanner.nextInt();
+        iScanner.close(); 
+        return number;
+    }
 }
