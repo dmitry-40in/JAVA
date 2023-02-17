5e3349abc686a66419f213fc0aa8eec17a607574
Dmitry <dmitry_40in@mail.ru>
2023-02-17 17:33:02 +0300

    method result added

--- hw_02/task_002.java
+++ hw_02/task_002.java
@@ -50,4 +50,30 @@ public class task_002 {
         data.close();
         return arguments;
     }
+
+    static String result(Map<String, Integer> arg) {
+
+        int aNumber = arg.get("a");
+        int bNumber = arg.get("b");
+        int result = 1;
+        String res;
+        if (bNumber > 0) {
+            for (int i = 1; i <= bNumber; i++) {
+                result *= aNumber;
+            }
+            res = String.valueOf(result);
+        } else if (bNumber == 0 && aNumber != 0) {
+            res = String.valueOf(result);
+        } else if (aNumber == 0 && bNumber == 0) {
+            res = "Не определено";
+        } else {
+            bNumber = bNumber * (-1);
+            for (int i = 1; i <= bNumber; i++) {
+                result *= aNumber;
+            }
+            double resultDouble = (double) 1 / result;
+            res = String.valueOf(resultDouble);
+        }
+        return res;
+    }
 }
