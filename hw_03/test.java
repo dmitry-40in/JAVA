e129121ab693d10d1d54bb0eddcce1d66d9a53d6
Dmitry <dmitry_40in@mail.ru>
2023-02-22 00:01:47 +0300

    test.java added

---
+++ hw_03/test.java
@@ -0,0 +1,9 @@
+public class test {
+    public static void main(String[] args) {
+        recursion();
+    }
+    static void recursion() {
+        System.out.println("***");
+        recursion();
+    }
+}
