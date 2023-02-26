d3c3e4a79b3eb84c5cc1f244e7638bd2a9c1f5cf
Dmitry <dmitry_40in@mail.ru>
2023-02-26 09:44:58 +0300

    heapSort added, try to add method heapify

--- hw_04/task_004.java
+++ hw_04/task_004.java
@@ -1,6 +1,14 @@
 public class task_004 {
 
     public static void main(String[] args) {
-        
+        int[] array = {19, 2000, -1, 34, 123, 0, -3245, 5, 8};
     }
+
+    private static void heapSort(int[] arr) {
+        int arrayLength = arr.length;
+        for (int i = arrayLength / 2 - 1; i >= 0; i--) {
+            heapify(arr, i, arrayLength);
+        }
+    }
+
 }
