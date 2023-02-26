250323816ed13d9d0c3802684a01ef855acde866
Dmitry <dmitry_40in@mail.ru>
2023-02-26 10:33:14 +0300

    printArray method added

--- hw_04/task_004.java
+++ hw_04/task_004.java
@@ -4,13 +4,25 @@ public class task_004 {
         int[] array = {19, 2000, -1, 34, 123, 0, -3245, 5, 8};
 
         heapSort(array);
+        printArray(array);
     }
 
     private static void heapSort(int[] arr) {
         int arrayLength = arr.length;
+        int tmp;
+
         for (int i = arrayLength / 2 - 1; i >= 0; i--) {
             heapify(arr, i, arrayLength);
         }
+
+        for (int i = arrayLength - 1; i >= 0; i--) {
+            tmp = arr[i];
+            arr[i] = arr[0]; 
+            arr[0] = tmp;
+
+            heapify(arr, 0, i);
+        }
+
     }
 
     private static void heapify(int[] arr, int i, int arrLength) {
@@ -31,9 +43,16 @@ public class task_004 {
             tmp = arr[i];
             arr[i] = arr[largestIndex];
             arr[largestIndex] = tmp;
+
+            heapify(arr, largestIndex, arrLength); // посмотерть как при рекурсии эта строка работает с ограничениями heapSort
         }
 
-        heapify(arr, largestIndex, arrLength); // посмотерть как при рекурсии эта строка работает с ограничениями heapSort
+    }
+
+    private static void printArray(int[] arr) {
+        for (int i : arr) {
+            System.out.printf("%d ", i);
+        }
     }
 
 }
