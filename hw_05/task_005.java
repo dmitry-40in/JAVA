0db79aacd9493a92637a80a5e119b0d43ff16e12
Dmitry <dmitry_40in@mail.ru>
2023-02-28 21:48:25 +0300

    method mapStartingPointMaker added

--- hw_05/task_005.java
+++ hw_05/task_005.java
@@ -1,11 +1,14 @@
 public class task_005 {
     public static void main(String[] args) {
 
-        int[][] m = mapMaking();
+        int[][] m = mapMaker();
         mapPrinting(m);
+        System.out.println();
+        mapPrinting(mapStartingPointMaker(m));
+        
     }
 
-    private static int[][] mapMaking() {
+    private static int[][] mapMaker() {
         int[][] map = {
                 { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 },
                 { -1, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, -1 },
@@ -34,9 +37,21 @@ public class task_005 {
     private static void mapPrinting(int[][] map) {
         for (int i = 0; i < map.length; i++) {
             for (int j = 0; j < map[i].length; j++) {
-                System.out.printf("%d  ", map[i][j]);
+                System.out.printf("%2d  ", map[i][j]);
             }
             System.out.println();
         }
+    }
+
+    private static int[][] mapStartingPointMaker(int[][] mapWithStartingPoint) {
+        System.out.println("Введите координаты начального расположения котика:");
+        // Введение координат с клавиатуры
+        
+        int column = 6;
+        int row = 5;
+
+        mapWithStartingPoint[column][row] = 1;
+
+        return mapWithStartingPoint;
     }
 }
