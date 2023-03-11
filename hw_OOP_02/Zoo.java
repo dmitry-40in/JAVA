e0ae73a5b6be73d04edcb16ba22fe84e75c20949
Dmitry <dmitry_40in@mail.ru>
2023-03-11 18:42:30 +0300

    try to add arraylist in class...

--- hw_OOP_02/Zoo.java
+++ hw_OOP_02/Zoo.java
@@ -1,3 +1,37 @@
+import java.util.ArrayList;
+
 public class Zoo {
+    ArrayList<Animal> animalsArray;
+
+    public Zoo(ArrayList<Animal> animalsArray) {
+        this.animalsArray = new ArrayList<Animal>;
+    }
     
+    public void addZooResident(Animal zooResident) {
+        this.animalsArray.add(zooResident);
+    }
+
+    public void removeZooResidentI(Animal zooResident) {
+        this.animalsArray.remove(zooResident);
+    }
+
+    public void showInfoAboutZooResidentI(Animal zooResident) {
+        this.animalsArray.remove(zooResident);
+    }
+
+    public void makeSoundZooResidentI(Animal zooResident) {
+        this.animalsArray.remove(zooResident);
+    }
+
+    public void printInfoAllZooResidents(Animal zooResident) {
+        this.animalsArray.remove(zooResident);
+    }
+
+    public void makeSoundAllZooResidents(Animal zooResident) {
+        this.animalsArray.remove(zooResident);
+    }
+    
+    
+    
+
 }
