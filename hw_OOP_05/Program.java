17683796ee4eb3252ba34eeca1076e595490c117
Dmitry <dmitry_40in@mail.ru>
2023-03-23 00:39:46 +0300

    from RPN doing

--- hw_OOP_05/Program.java
+++ hw_OOP_05/Program.java
@@ -1,71 +1,21 @@
 import java.util.Stack;
 
-public class Program {
+
+public class Program extends RPN{
     public static void main(String[] args) {
 
-        String input = "(1+2*(3+4*(5-6)))/(7-8-9)+10";
-        System.out.println(input);
+               
+        RPN e = new RPN();
+        String r = e.getRPN("11+22+33");
+        
+        System.out.println(r);
+    
+        double s = e.getResult("11 22 +33+");
 
-        Stack<Character> stack = new Stack<Character>();
-        StringBuilder expression = new StringBuilder();
-     
-        expression.append("");
-        stack.clear();
-
-        for (int i = 0; i < input.length(); i++) {
-            if (input.charAt(i) == '(') {
-                stack.push(input.charAt(i));
-            } else if (input.charAt(i) == ')') {
-                while (stack.peek() != '(') {
-                    expression.append(stack.pop());
-                }
-                stack.pop();
-            } else if (input.charAt(i) == '+' || input.charAt(i) == '-') {
-                if (!stack.isEmpty()) {
-                    if (stack.peek() == '+' || stack.peek() == '-') {
-                        expression.append(stack.pop());
-                        stack.push(input.charAt(i));
-                    } else if (stack.peek() == '*' || stack.peek() == '/') {
-                        while (!stack.isEmpty() && stack.peek() != '(') {
-                            expression.append(stack.pop());
-                        }
-                        stack.push(input.charAt(i));
-                    } else {
-                        stack.push(input.charAt(i));
-                    }
-                } else {
-                    stack.push(input.charAt(i));
-                }
-            } else if (input.charAt(i) == '*' || input.charAt(i) == '/') {
-                if (!stack.isEmpty()) {
-                    if (stack.peek() == '+' || stack.peek() == '-') {
-                        stack.push(input.charAt(i));
-                    } else if (stack.peek() == '*' || stack.peek() == '/') {
-                        expression.append(stack.pop());
-                        stack.push(input.charAt(i));
-
-                    } else {
-                        stack.push(input.charAt(i));
-                    }
-
-                } else {
-                    stack.push(input.charAt(i));
-                }
-
-            } else {
-                expression.append(input.charAt(i));
-            }
-
-            if (i == input.length() - 1) {
-                while (stack.isEmpty() != true) {
-                    expression.append(stack.pop());
-                }
-            }
-        }
-
-        System.out.println(stack);
-        System.out.println(expression);
+        System.out.println(s);
 
     }
-
+      
+    
 }
+       
---
+++ hw_OOP_05/RPN.java
@@ -0,0 +1,130 @@
+import java.util.Stack;
+
+public class RPN {
+
+    public String getRPN(String inputExpression) {
+
+        Stack<Character> stack = new Stack<Character>();
+        StringBuilder expressionRPN = new StringBuilder();
+
+        expressionRPN.append("");
+        stack.clear();
+
+        for (int i = 0; i < inputExpression.length(); i++) {
+            // if (inputExpression.charAt(i) == '+' || inputExpression.charAt(i) == '-' ||
+            // inputExpression.charAt(i) == '*' || inputExpression.charAt(i) == '/') {
+            // expressionRPN.append(' ');
+            // }
+
+            if (inputExpression.charAt(i) == '(') {
+                stack.push(inputExpression.charAt(i));
+            } else if (inputExpression.charAt(i) == ')') {
+                expressionRPN.append(' '); //////////////////////////////////////////////////////////////////
+                while (stack.peek() != '(') {
+                    expressionRPN.append(stack.pop());
+                }
+                stack.pop();
+            } else if (inputExpression.charAt(i) == '+' || inputExpression.charAt(i) == '-') {
+                expressionRPN.append(' '); //////////////////////////////////////////////////////////////////
+                if (!stack.isEmpty()) {
+                    if (stack.peek() == '+' || stack.peek() == '-') {
+                        expressionRPN.append(stack.pop());
+                        stack.push(inputExpression.charAt(i));
+                    } else if (stack.peek() == '*' || stack.peek() == '/') {
+                        while (!stack.isEmpty() && stack.peek() != '(') {
+                            expressionRPN.append(stack.pop());
+                        }
+                        stack.push(inputExpression.charAt(i));
+                    } else {
+                        stack.push(inputExpression.charAt(i));
+                    }
+                } else {
+                    stack.push(inputExpression.charAt(i));
+                }
+            } else if (inputExpression.charAt(i) == '*' || inputExpression.charAt(i) == '/') {
+                expressionRPN.append(' '); //////////////////////////////////////////////////////////////////
+                if (!stack.isEmpty()) {
+                    if (stack.peek() == '+' || stack.peek() == '-') {
+                        stack.push(inputExpression.charAt(i));
+                    } else if (stack.peek() == '*' || stack.peek() == '/') {
+                        expressionRPN.append(stack.pop());
+                        stack.push(inputExpression.charAt(i));
+
+                    } else {
+                        stack.push(inputExpression.charAt(i));
+                    }
+
+                } else {
+                    stack.push(inputExpression.charAt(i));
+                }
+
+            } else {
+                expressionRPN.append(inputExpression.charAt(i));
+            }
+
+            if (i == inputExpression.length() - 1) {
+                while (stack.isEmpty() != true) {
+                    expressionRPN.append(stack.pop());
+                }
+            }
+        }
+
+        System.out.println(inputExpression);
+        System.out.println(stack);
+        System.out.println(expressionRPN);
+        System.out.println();
+
+        // String result = expressionRPN.toString();
+        return expressionRPN.toString();
+    }
+
+    public Double getResult(String expRPN) {
+        Double result = 0.0;
+        StringBuilder tmp = new StringBuilder();
+        String tmpString = new String();
+        Double one = 0.0;
+        Double two = 0.0;
+
+        tmp.append("");
+        tmpString = "";
+
+        Stack<Double> resultStack = new Stack<Double>();
+
+        for (int i = 0; i < expRPN.length(); i++) {
+            if (expRPN.charAt(i) == '1' || expRPN.charAt(i) == '2' || expRPN.charAt(i) == '3' || expRPN.charAt(i) == '4'
+                    || expRPN.charAt(i) == '5' || expRPN.charAt(i) == '6' || expRPN.charAt(i) == '7'
+                    || expRPN.charAt(i) == '8' || expRPN.charAt(i) == '9' || expRPN.charAt(i) == '0'
+                    || expRPN.charAt(i) == '.') {
+                tmp.append(expRPN.charAt(i));
+            } else if (expRPN.charAt(i) == ' ' || expRPN.charAt(i) == '+' || expRPN.charAt(i) == '-' || expRPN.charAt(i) == '*' || expRPN.charAt(i) == '/') {
+                tmpString = tmp.toString();
+                resultStack.push(Double.parseDouble(tmpString));
+                tmp.setLength(0);
+                tmpString = "";
+
+                System.out.println(resultStack);
+
+                if (expRPN.charAt(i) != ' ') {
+                    one = resultStack.pop();
+                    two = resultStack.pop();
+                    if (expRPN.charAt(i) == '+') {
+                        resultStack.push(one + two);
+                    } else if (expRPN.charAt(i) == '-') {
+                        resultStack.push(one - two);
+                    } else if (expRPN.charAt(i) == '*') {
+                        resultStack.push(one * two);
+                    } else {
+                        resultStack.push(one / two);
+                    }
+                }
+                
+            }
+        }
+
+        result = resultStack.pop();
+
+        return result;
+
+    }
+
+}
