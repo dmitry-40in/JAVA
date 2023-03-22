1aead6626b3a9c4d6451e3124aff7ffca75faa85
Dmitry <dmitry_40in@mail.ru>
2023-03-23 02:52:09 +0300

    Calculator done

--- hw_OOP_05/Program.java
+++ hw_OOP_05/Program.java
@@ -1,11 +1,14 @@
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Program extends RPN {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         boolean menu = true;
         Scanner scanner = new Scanner(System.in);
-        RPN e = new RPN();
+        RPN expression = new RPN();
+        FileWriter log = new FileWriter("log.txt");
 
         while (menu) {
             System.out.println();
@@ -19,15 +22,21 @@ public class Program extends RPN {
                 System.out.printf("Введите выражение без пробелов: ");
                 String input = scanner.next();
                 System.out.println();
-                String r = e.getRPN(input);
+                String rpn = expression.getRPN(input);
 
-                System.out.printf("Ответ: %f\n", e.getResult(r));
+                log.write("The entered expression: " + input + "\n");
+                log.write("Expression in the RPN: " + rpn + "\n");
+                log.write("Calculation result: " + expression.getResult(rpn) + "\n\n");
+
+                System.out.printf("Ответ: %f\n", expression.getResult(rpn));
+
 
             } else {
                 System.out.println("Досвидания!");
                 break;
             }
         }
+        log.close();
         scanner.close();
     }
 
